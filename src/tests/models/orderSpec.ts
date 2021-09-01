74afaef3a9c722fd2f187ec27f7314b682afc430
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-01 22:50:08 +1000

    tests: added tests for get current and complete order for user

--- src/tests/models/orderSpec.ts
+++ src/tests/models/orderSpec.ts
@@ -27,42 +27,13 @@ describe('Order Model', () => {
   it('should have a delete method', () => {
     expect(store.delete).toBeDefined();
   });
+
+  it('should have a delete method', () => {
+    expect(store.getCurrentOrderByUserId).toBeDefined();
+  });
+
+  it('should have a delete method', () => {
+    expect(store.getCompletedOrdersByUserId).toBeDefined();
+  });
+
 });
-
-// it('create method should add an order', async () => {
-//   const result = await store.create({
-//     user_id: '1',
-//     status: 'active'
-//   });
-//   expect(result).toEqual({
-//     id: 1,
-//     user_id: '1',
-//     status: 'active'
-//   });
-// });
-
-// it('index method should return a list of orders', async () => {
-//   const result = await store.index();
-//   expect(result).toEqual([
-//     {
-//       id: 1,
-//       user_id: '1',
-//       status: 'active'
-//     }
-//   ]);
-// });
-
-// it('show method should return the correct order', async () => {
-//   const result = await store.show(1);
-//   expect(result).toEqual({
-//     id: 1,
-//     user_id: '1',
-//     status: 'active'
-//   });
-// });
-
-// it('delete method should remove the order', async () => {
-//   store.delete(1);
-//   const result = await store.index();
-//   expect(result).toEqual([]);
-// });
